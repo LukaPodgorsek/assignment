f34da17a7530314ac9b33943a115cefabba637e9
LukaPodgorsek <lp5796@student.uni-lj.si>
2021-08-19 18:02:18 +0200

    added component framework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+//import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Libs
+import { Grommet } from "grommet";
+import { hpe } from "grommet-theme-hpe";
+class App extends Component {
+  render() {
+    return <Grommet theme={hpe} full></Grommet>;
+  }
 }
+
+App.propTypes = {};
 
 export default App;
